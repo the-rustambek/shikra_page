02036adbe3064a22406c5833cf83b29925183a48
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-06-09 07:01:12 -0700

    add file

---
+++ main.js
@@ -0,0 +1,47 @@
+let firstChart = document.getElementById('myChart').getContext('2d');
+
+let barChart = new Chart(firstChart, {
+  type: 'bar',
+  data: {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    datasets: [{
+      barPercentage: 0.5,
+      barThickness: 15,
+      maxBarThickness: 20,
+      minBarLength: 2, 
+      label: "Revenue",
+      borderRadius: 500,
+      data: [
+        400,
+        500,
+        300,
+        240,
+        700,
+        600,
+        700
+      ],
+      backgroundColor: "#F86549"
+    }]
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Guests',
+        fontSize: 30
+      },
+    }
+  }
+})
+
+
+
+
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider' ).mount();
+} );
